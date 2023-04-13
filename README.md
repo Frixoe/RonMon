d2286b3ef965230453337f50eee4be3bb038ec58
Suryansh <suryansh5060@gmail.com>
2023-04-13 22:46:26 +0530

    Initial files

---
+++ README.md
@@ -0,0 +1,13 @@
+# RonMon
+
+A tool to monitor a Ronin Validator. Fork of ArjunSomvanshi's [zillmon](https://github.com/Arjun-Somvanshi/zillmon).
+
+## Installation
+
+- Clone the repository
+- Install all the dependencies
+- Create a `.env` file with the following variables: 
+    - `BOT_TOKEN`: API Key for the telegram bot
+    - `VALD_URL`: The validator's RPC url
+    - `BRIDGE_OPERATOR_ADDRESS`: Bridge operator's public address
+    - `VALIDATOR_NODE_ADDRESS`: Validator node's public address
---
+++ main.py
@@ -0,0 +1,139 @@
+import json
+import requests
+import os
+import telebot
+import time
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+bot = telebot.TeleBot(BOT_TOKEN)
+
+class RonMon:
+    def __init__(self):
+        self.peer_count_vald = None
+
+    def read_config(self):
+        try:
+            with open(os.path.expanduser("~") + os.path.sep + ".config/ronmon/ron.json", "r") as f:
+                self.config = json.load(f)
+        except Exception as e:
+            self.config = {"vald_url": os.getenv("VALD_URL"),
+                           "remote_url": "https://api.roninchain.com/rpc",
+                           "chat_id": 1911362325
+                          }
+            print(f"Error: {e}")
+            print("Loading default config...")
+
+    def rpc_call(self, url, data):
+        headers = {"Content-Type": "application/json"}
+        data = json.dumps(data)
+        r = requests.post(url, data=data, headers=headers)
+        return json.loads(r.text)
+
+    def get_bridge_operator_balance(self):
+        data = {
+                "id": "1",
+                "jsonrpc": "2.0",
+                "method": "eth_getBalance",
+                "params": [ os.getenv("BRIDGE_OPERATOR_ADDRESS"), "latest" ],
+                "id": 1
+               }
+        self.validator_balance_vald = int(self.rpc_call(self.config["vald_url"], data)["result"], 16)
+        self.validator_balance_remote = []
+        self.validator_balance_remote = int(self.rpc_call(self.config["remote_url"], data)["result"], 16)
+
+    def get_current_block(self):
+        data = {
+                "id": "1",
+                "jsonrpc": "2.0",
+                "method": "eth_blockNumber",
+                "params": [],
+                "id": 83
+               }
+        self.current_block_vald = int(self.rpc_call(self.config["vald_url"], data)["result"], 16)
+        self.current_block_remote = int(self.rpc_call(self.config["remote_url"], data)["result"], 16)
+
+    def get_peer_count(self):
+        data = {
+                "id": "1",
+                "jsonrpc": "2.0",
+                "method": "net_peerCount",
+                "params": [],
+                "id": 74
+               }
+        self.peer_count_vald = int(self.rpc_call(self.config["vald_url"], data)["result"], 16)
+
+    def get_blockchain_info_from_validator(self):
+        return { current_block: self.current_block_vald, peer_count: self.peer_count_vald, bridge_operator_balance: self.validator_balance_vald }
+
+    def get_blockchain_info(self):
+        return { current_block: self.current_block_remote, peer_count: self.peer_count_vald, bridge_operator_balance: self.validator_balance_remote }
+
+    def alerts_BalanceLow(self):
+        if self.validator_balance_remote < 150:
+            bot.send_message(self.config["chat_id"], f'''Ronin Bridge Operator Balance is Low!\n
+                              Bridge Operator Balance: {self.validator_balance_remote}
+                              ''')
+        print(f'''Ronin Bridge Operator Balance is Low!\n
+                          Bridge Operator Balance: {self.validator_balance_remote}
+                          ''')
+
+    def alert_BlockNum(self):
+        if self.current_block_vald < self.current_block_remote - 5:
+            bot.send_message(self.config["chat_id"], f'''Block Height of Ronin Validator is Lagging\n
+                              Validator Height: {self.current_block_vald}
+                              Remote Node Height: {self.current_block_remote}
+                              ''')
+        print(f'''Block Height of Ronin Validator is Lagging\n
+                          Validator Height: {self.current_block_vald}
+                          Remote Node Height: {self.current_block_remote}
+                          ''')
+
+    def alert_DeficitPeers(self):
+        if self.peer_count_vald < 5:
+            bot.send_message(self.config["chat_id"], f'''Ronin Validator Peer Count Below 5
+                              Validator Peers: {self.peer_count_vald}
+                              ''')
+        print(f'''Ronin Validator Peer Count Below 5
+                          Validator Peers: {self.peer_count_vald}
+                          ''')
+
+    def monitor(self):
+        while True:
+            # self.get_peer_count()
+            self.get_current_block()
+            self.get_bridge_operator_balance()
+            self.alert_BlockNum()
+            # self.alert_DeficitPeers()
+            self.alerts_BalanceLow()
+            time.sleep(60)
+
+
+ron = RonMon()
+ron.read_config()
+
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+        Hi there, I am RonMon.
+        I am here to monitor you Ronin Validator
+    """)
+
+@bot.message_handler(commands=['status'])
+def send_status(message):
+    status = ron.get_blockchain_info_from_validator()
+    bot.reply_to(message, "Validator Status: \n" + str(status))
+
+@bot.message_handler(commands=['rstatus'])
+def send_remote_status(message):
+    status = ron.get_blockchain_info()
+    bot.reply_to(message, "Remote Node Status: \n" + str(status))
+
+def start_monitoring():
+    bot.send_message(ron.config["chat_id"], f"Monitoring Validator: {ron.config['vald_url']}")
+    ron.monitor()
+
+start_monitoring()
+bot.infinity_polling()
